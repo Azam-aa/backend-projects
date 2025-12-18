38945062aacdd642ef4d0f2659638342d50c3611
Azam-aa <azamp442@gmail.com>
2025-12-18 17:04:16 +0530

    Hello API project

---
+++ helllo-api/helllo-api/src/main/java/com/azam/helloapi/HellloApiApplication.java
@@ -0,0 +1,13 @@
+package com.azam.helloapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellloApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellloApiApplication.class, args);
+	}
+
+}
---
+++ helllo-api/helllo-api/src/test/java/com/azam/helloapi/HellloApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azam.helloapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellloApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
